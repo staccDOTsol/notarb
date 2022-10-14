c4216fb7954a95874324eccbdd32f14014ed4213
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-14 20:26:23 -0300

    hi

--- index.mjs
+++ index.mjs
@@ -74,7 +74,7 @@ var mints = [
   "Ea5SjE2Y6yvCeW5dYTn7PYMuW5ikXkvbGdcmSnXeaLjS",
   "7kbnvuGBxxj8AG9qp8Scn56muWGaRaFqxg1FsRp3PaFT"]
 for (var add of somestuff2.data){
-for (var address of add.tokens){
+for (var address of add.token){
 mints.push(address.address)
 }
 }
