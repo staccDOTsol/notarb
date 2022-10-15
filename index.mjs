a6e55981a70925d4689756e8c58acd413d828c8a
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-15 01:04:39 -0300

    hi

--- index.mjs
+++ index.mjs
@@ -244,13 +244,13 @@ for (var b in Object.keys(toArr[0])){
 // 0.1 SOL
 await prism.loadRoutes( SOL_MINT, USDC_MINT, true ); 
 
-let routes2  = prism.getRoutes((Math.floor(routes[0].amountOut* 0.995 * 10 ** 6) / 10 ** 6 ) );
-let returns = ((routes2[0].amountOut / (initial / 10 ** dec)) - 1) * 100
+let routes2  = prism.getRoutes((Math.floor(routes[0].amountWithFees* 0.995 * 10 ** 6) / 10 ** 6 ) );
+let returns = ((routes2[0].amountWithFees / (initial / 10 ** dec)) - 1) * 100
 let gogo = true 
 for (var maybego of  dothethings){
   gogo = maybego
 }
-if (returns > 0.01 && gogo){
+if (returns > 0.00001 && gogo){
   
   if (true){
   // when outAmount more than initial
