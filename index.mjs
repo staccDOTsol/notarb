d4534f2986c31c82229dceec6dc4938f917f03b1
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-15 01:50:46 -0300

    lol this is not a fix at all cc @jstrry

--- index.mjs
+++ index.mjs
@@ -309,7 +309,7 @@ let gogo = true
 for (var maybego of  dothethings){
   gogo = maybego
 }
-if (returns > 0.00001 && gogo){
+if (returns > 0.004 && gogo){
   
   if (true){
   // when outAmount more than initial
