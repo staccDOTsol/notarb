cd98848edabbc17d3915026b1338ba5659da407f
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-15 20:17:09 -0300

    b

--- deathtomango.mjs
+++ deathtomango.mjs
@@ -37,7 +37,7 @@ console.log({ dotenv });
 dotenv.config();
 // This is a free Solana RPC endpoint. It may have ratelimit and sometimes
 // invalid cache. I will recommend using a paid RPC endpoint.
-const connection = new Connection("http://69.46.29.78:8899", {skipPreflight: true});
+const connection = new Connection("http://69.46.29.78:8899", {skipPreflight: false});
 const connection2 = new Connection("https://solana-mainnet.g.alchemy.com/v2/Zf8WbWIes5Ivksj_dLGL_txHMoRA7-Kr", {skipPreflight: true});
 const wallet = new Wallet(
   Keypair.fromSecretKey(new Uint8Array(JSON.parse(fs.readFileSync('/Users/jarettdunn/notjaregm.json').toString()))));
