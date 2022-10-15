8d43eb942c2425164779474916e4e0c38956bdaf
Jarett Dunn <jarettrsdunn+git@gmail.com>
2022-10-15 16:31:10 +0000

    haa

--- jupjup.mjs
+++ jupjup.mjs
@@ -295,6 +295,7 @@ let min = ( reserve.stats.borrowFeePercentage * 100)
        if (usdcToSol.data[0] && !baddies.includes(SOL_MINT+USDC_MINT) ){
         try {
 
+          await sleep(Math.random()*(Math.random()*500)+666)
          solToUsdc = await getCoinQuote(
           SOL_MINT,
           USDC_MINT,
