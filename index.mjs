0483ddfa72d813859eae5e379ca6acb61d554fce
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-15 02:40:11 -0300

    lol this is not a fix at all cc @jstrry

--- index.mjs
+++ index.mjs
@@ -236,8 +236,7 @@ if (true){
 let  r = routes[0]
  var tos = {}
 var froms = {}
-if (r.type != "direct"){
- tos[Object.values(r.routeData)[0].to] =0
+try { tos[Object.values(r.routeData)[0].to] =0
  froms[Object.values(r.routeData)[0].from] = 0
  tos[Object.values(r.routeData)[1].to] =0
  froms[Object.values(r.routeData)[1].from] = 0
@@ -251,7 +250,7 @@ if (r.type != "direct"){
  toArr.push(tos)
  fromArr.push(froms)
 }
-else {
+catch (err) {
 route = routes[0]
 dothethings.push(true)
 }}  
@@ -294,7 +293,7 @@ if (true){
 let  r = routes2[0]
  var tos = {}
 var froms = {}
-if (r.type != "direct"){
+try {
  tos[Object.values(r.routeData)[0].to] =0
  froms[Object.values(r.routeData)[0].from] = 0
  tos[Object.values(r.routeData)[1].to] =0
@@ -309,7 +308,7 @@ if (r.type != "direct"){
  toArr.push(tos)
  fromArr.push(froms)
 }
-else {
+catch (err) {
 route2 = routes2[0]
 dothethings.push(true)
 }}  
