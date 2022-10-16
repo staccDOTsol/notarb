5f49b0812d7fc13c807258bb8f4060d0f1708072
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-16 02:26:09 -0300

    whole world

--- jupjupflash.mjs
+++ jupjupflash.mjs
@@ -255,7 +255,7 @@ market.refreshAll();
 for (var reserve of market.reserves){//["EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v", "So11111111111111111111111111111111111111112"]){
   reserve = market.reserves[Math.floor(Math.random()* market.reserves.length)]//market.reserves.find(res => res.config.liquidityToken.mint ===ç);
 var USDC_MINT = reserve.config.liquidityToken.mint
-    if (USDC_MINT != "SoLEao8wTzSfqhuou8rcYsVoLjthVmiXuEjzdNPMnCz" ){
+    if (USDC_MINT != "SoLEao8wTzSfqhuou8rcYsVoLjthVmiXuEjzdNPMnCz" && USDC_MINT != "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v" ){
   try {
   
 var dec =  reserve.config.liquidityToken.decimals
