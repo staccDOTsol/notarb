f6b78b9817d3446cd44adbfd0576ffc2ff130e29
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-17 12:01:34 -0300

    megaflash

--- jupjupflash.mjs
+++ jupjupflash.mjs
@@ -371,7 +371,7 @@ let gogo = true
 for (var maybego of  dothethings){
   gogo = maybego
 }
-if (returns > -0.25 && gogo){
+if (returns > -0.001 && gogo){
   
   if (true){
   // when outAmount more than initial
