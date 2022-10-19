dda2c65495bc86b01bbcbaf5fa31d53600bcb5ac
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-18 23:27:23 -0300

    ge

--- jupjupflash.mjs
+++ jupjupflash.mjs
@@ -38,39 +38,6 @@ console.log({ dotenv });
 dotenv.config();
 import { PromisePool }from '@supercharge/promise-pool'
 
-setInterval(async function(){
-  try {
-    let  connection = new Connection((process.env.NODE_ENV == 'production' ? 'http://localhost' : 'http://localhost') +":8899", {skipPreflight: false});
-    const connection2 = new Connection("https://solana-mainnet.g.alchemy.com/v2/Zf8WbWIes5Ivksj_dLGL_txHMoRA7-Kr", {skipPreflight: false});
-   connection = connection2
-
-    let luts = await connection.getProgramAccounts(AddressLookupTableProgram.programId)
-   // console.log(luts)
-    await PromisePool.withConcurrency(50)
-    .for(luts)
-    // @ts-ignore
-    .handleError(async (err, asset) => {
-      console.error(`\nError uploading or whatever`, err.message);
-      console.log(err);
-    })
-    // @ts-ignore
-    .process(async (lut) => {
-      let maybemine = await connection2.getAddressLookupTable(lut.pubkey)
-      if (maybemine.value?.state.authority?.toBase58()== ("5kqGoFPBGoYpFcxpa6BFRp3zfNormf52KCo5vQ8Qn5bx"))
-      {
-
-        let temp = ""
-        for (var abc of maybemine.value.state.addresses){
-          temp+=(abc.toBase58() + ",")
-        }
-     myluts[ temp ] = lut.pubkey
-      }
-    })
-    fs.writeFileSync('./luts.json', JSON.stringify(myluts))
-  } catch (err){
-
-  }
-}, 5 *  60000)
 // This is a free Solana RPC endpoint. It may have ratelimit and sometimes
 // invalid cache. I will recommend using a paid RPC endpoint.
 let  connection = new Connection((process.env.NODE_ENV == 'production' ? 'http://localhost' : 'http://localhost') +":8899", {skipPreflight: false});
