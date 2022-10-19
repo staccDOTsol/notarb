3524b4bf00baa7aff585874a959a734c112886ac
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-19 14:19:17 -0300

    jacked the keys to her fcking car

--- getmyluts.ts
+++ getmyluts.ts
@@ -3,11 +3,14 @@ const PromisePool = require("@supercharge/promise-pool").default;
 
 import fs from 'fs'
 setTimeout(async function(){
-  const connection2 = new Connection("https://solana-mainnet.g.alchemy.com/v2/Zf8WbWIes5Ivksj_dLGL_txHMoRA7-Kr");
-  var payer = (
+  // @ts-ignore
+  let  connection = new Connection((process.env.NODE_ENV == 'production' ? 'http://localhost' : 'http://localhost') +":8899", {skipPreflight: false});
+  // @ts-ignore
+  const connection2 = new Connection("https://solana-mainnet.g.alchemy.com/v2/Zf8WbWIes5Ivksj_dLGL_txHMoRA7-Kr", {skipPreflight: false});
+  //connection = connection2
+    var payer = (
     Keypair.fromSecretKey(new Uint8Array(JSON.parse(fs.readFileSync((process.env.NODE_ENV == 'production' ? '/home/ubuntu' : '/home/ubuntu') + '/notjaregm.json').toString()))));
   
-      var connection =  new Connection("https://solana-mainnet.g.alchemy.com/v2/Zf8WbWIes5Ivksj_dLGL_txHMoRA7-Kr")
 const configOrCommitment: GetProgramAccountsConfig = {
     commitment: 'confirmed',
     filters: [
