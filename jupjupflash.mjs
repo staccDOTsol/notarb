e0e61b480b5b779fe1d0e9ea4a8c4794bac5f4a1
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-19 12:22:49 -0300

    tell me can't you feel

--- jupjupflash.mjs
+++ jupjupflash.mjs
@@ -256,7 +256,7 @@ let min = ( reserve.stats.borrowFeePercentage * 100)
     let cba = -1
     abc++
 
-        if (min < 1){//!baddies.includes(SOL_MINT+USDC_MINT) &&  !baddies.includes(USDC_MINT+SOL_MINT) && min < 1){
+        if (!baddies.includes(SOL_MINT+USDC_MINT) &&  !baddies.includes(USDC_MINT+SOL_MINT) && min < 1){
           console.log(min)
 
       let dothethings = []
