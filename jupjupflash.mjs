6cdf1e1d0517c808e51f7d2a8b62915acc7e2b59
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-19 19:07:51 -0300

    jacked the keys to her fcking car

--- jupjupflash.mjs
+++ jupjupflash.mjs
@@ -48,9 +48,9 @@ const connection2 = new Connection("https://solana-mainnet.g.alchemy.com/v2/Zf8W
 //connection = connection2
 
 const wallet = new Wallet(
-  Keypair.fromSecretKey(new Uint8Array(JSON.parse(fs.readFileSync((process.env.NODE_ENV == 'production' ? '/home/ubuntu' : '/Users/jarettdunn') + '/notjaregm.json').toString()))));
+  Keypair.fromSecretKey(new Uint8Array(JSON.parse(fs.readFileSync((process.env.NODE_ENV == 'production' ? '/home/ubuntu' : '/home/ubuntu') + '/notjaregm.json').toString()))));
   const payer = (
-    Keypair.fromSecretKey(new Uint8Array(JSON.parse(fs.readFileSync((process.env.NODE_ENV == 'production' ? '/home/ubuntu' : '/Users/jarettdunn') + '/notjaregm.json').toString()))));
+    Keypair.fromSecretKey(new Uint8Array(JSON.parse(fs.readFileSync((process.env.NODE_ENV == 'production' ? '/home/ubuntu' : '/home/ubuntu') + '/notjaregm.json').toString()))));
   
 import fs from 'fs'
 import { createTransferInstruction } from "@solana/spl-token";
