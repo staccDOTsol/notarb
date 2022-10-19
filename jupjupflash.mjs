ee021ef684c512dc3d612af8755598998b6ceeff
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-19 12:35:26 -0300

    ge

--- jupjupflash.mjs
+++ jupjupflash.mjs
@@ -555,7 +555,7 @@ let ss = [new PublicKey(USDC_MINT), new PublicKey(SOL_MINT)]
 let aaa = 0
 let somestuff = {} 
 let test = ((await connection.getAddressLookupTable((winner))).value)
-ttt = test
+let ttt = test
 for (var bca of messageV0.staticAccountKeys){
   aaa++
 if (aaa < messageV0.staticAccountKeys.length / 3 * 2  && (aaa >= messageV0.staticAccountKeys.length / 3  )){
