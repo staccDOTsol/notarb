651188e3c842a8573f7672bd78a992c3cf529625
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-19 23:35:33 -0300

    ix

--- jupjup.mjs
+++ jupjup.mjs
@@ -44,7 +44,7 @@ import { PromisePool }from '@supercharge/promise-pool'
     let  connection = new Connection((process.env.NODE_ENV == 'production' ? 'http://localhost' : 'http://localhost') +":8899", {skipPreflight: false});
     const connection2 = new Connection("https://solana-mainnet.g.alchemy.com/v2/Zf8WbWIes5Ivksj_dLGL_txHMoRA7-Kr", {skipPreflight: false});
 
-    process.env.SEARCHER ? connection = connection2 : connection = connection
+    process.env.SEARCHER ? connection = connection2 : connection2 = connection
     
 const wallet = new Wallet(
   Keypair.fromSecretKey(new Uint8Array(JSON.parse(fs.readFileSync((process.env.NODE_ENV == 'production' ? '/home/ubuntu' : '/home/ubuntu') + '/notjaregm.json').toString()))));
