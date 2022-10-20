e88a6e39f0f7adb07f63e32fe2a942a6b9017979
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-19 23:54:45 -0300

    ix

--- jupjupflash.mjs
+++ jupjupflash.mjs
@@ -249,7 +249,7 @@ for (var reserve of market.reserves){//["EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwy
   try {
  reserve = market.reserves[Math.floor( rando(0, 1, "float")* market.reserves.length)]//market.reserves.find(res => res.config.liquidityToken.mint ===ç);
 var USDC_MINT = reserve.config.liquidityToken.mint
-
+console.log(USDC_MINT)
     if (true){//has.includes(USDC_MINT) ){
   
 var dec =  reserve.config.liquidityToken.decimals
