8c12c19da8c950f3a68e0ed7f97ffbdbb5dfa9bd
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-20 01:20:09 -0300

    luts

--- jupjup.mjs
+++ jupjup.mjs
@@ -53,9 +53,9 @@ process.env.SEARCHER ? connection2 = new Connection("https://solana-mainnet.g.al
 process.env.SEARCHER ? connection = connection2 : connection = connection
 
 const wallet = new Wallet(
-  Keypair.fromSecretKey(new Uint8Array(JSON.parse(fs.readFileSync((process.env.NODE_ENV == 'production' ? '/home/ubuntu' : '/Users/jarettdunn') + '/notjaregm.json').toString()))));
+  Keypair.fromSecretKey(new Uint8Array(JSON.parse(fs.readFileSync((process.env.NODE_ENV == 'production' ? '/home/ubuntu' : '/home/ubuntu') + '/notjaregm.json').toString()))));
   const payer = (
-    Keypair.fromSecretKey(new Uint8Array(JSON.parse(fs.readFileSync((process.env.NODE_ENV == 'production' ? '/home/ubuntu' : '/Users/jarettdunn') + '/notjaregm.json').toString()))));
+    Keypair.fromSecretKey(new Uint8Array(JSON.parse(fs.readFileSync((process.env.NODE_ENV == 'production' ? '/home/ubuntu' : '/home/ubuntu') + '/notjaregm.json').toString()))));
   
   
 import fs from 'fs'
