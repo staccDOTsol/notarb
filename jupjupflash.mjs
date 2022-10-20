fa3fe9402f4aafb4c345e99bbd8a57fcc306b172
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-20 19:01:13 -0300

    ahh

--- jupjupflash.mjs
+++ jupjupflash.mjs
@@ -63,28 +63,6 @@ process.on('uncaughtException', err => {
     })
   })
 })
-process.on('unhandledRejection', (reason, promise) => {
-
-
-   PromisePool.withConcurrency(1)
-  .for(markets)
-  // @ts-ignore
-  .process(async (market) => {
-    myluts = JSON.parse(fs.readFileSync("./luts.json").toString());
-    //await createWSolAccount();
-  
-        market = markets[Math.floor(rando(0, 1, "float") * markets.length)];
-    await market.loadReserves();
-    market.refreshAll();
-
-    await PromisePool.withConcurrency(1)
-    .for(mints)
-    // @ts-ignore
-    .process(async (SOL_MINT) => {
-     await something(SOL_MINT, market, myluts)
-    })
-  })
-})
 // This is a free Solana RPC endpoint. It may have ratelimit and sometimes
 // invalid cache. I will recommend using a paid RPC endpoint.
 let connection = new Connection(
@@ -6444,7 +6422,7 @@ async function something(SOL_MINT, market, myluts){
 while (true) {
 
 
-  await PromisePool.withConcurrency(markets.length)
+  await PromisePool.withConcurrency(5)
   .for(markets)
   // @ts-ignore
   .process(async (market) => {
