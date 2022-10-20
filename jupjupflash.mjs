8d6ed4c0acbabfc9fef9faa1d6551b27bbfb3201
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-20 07:54:23 -0300

    ugh gn

--- jupjupflash.mjs
+++ jupjupflash.mjs
@@ -44,7 +44,7 @@ import { PromisePool }from '@supercharge/promise-pool'
 
 // This is a free Solana RPC endpoint. It may have ratelimit and sometimes
 // invalid cache. I will recommend using a paid RPC endpoint.
-let  connection = new Connection((process.env.NODE_ENV == 'production' ? 'http://localhost:8899' : 'https://solana-mainnet.g.alchemy.com/v2/IWB_lF5cQVi-HfV19leFFMitqWKG2gp4'), {skipPreflight: false});
+let  connection = new Connection((process.env.NODE_ENV == 'production' ? 'https://solana-mainnet.g.alchemy.com/v2/IWB_lF5cQVi-HfV19leFFMitqWKG2gp4' : 'https://solana-mainnet.g.alchemy.com/v2/IWB_lF5cQVi-HfV19leFFMitqWKG2gp4'), {skipPreflight: false});
 var connection2= new Connection("https://solana-mainnet.g.alchemy.com/v2/IWB_lF5cQVi-HfV19leFFMitqWKG2gp4", {skipPreflight: false});
 process.env.SEARCHER ? connection2 = new Connection("https://solana-mainnet.g.alchemy.com/v2/Zf8WbWIes5Ivksj_dLGL_txHMoRA7-Kr") : connection2 = connection2
 
