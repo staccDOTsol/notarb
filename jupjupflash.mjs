50c2c13a3fecd0c4c4e157eac21693436d4f0503
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-22 18:15:13 -0300

    ahh

--- jupjupflash.mjs
+++ jupjupflash.mjs
@@ -6152,7 +6152,7 @@ while (true) {
     await market.loadReserves();
     market.refreshAll();
 
-    await PromisePool.withConcurrency(8)
+    await PromisePool.withConcurrency(56)
     .for(mints)
     // @ts-ignore
     .process(async (SOL_MINT) => {
