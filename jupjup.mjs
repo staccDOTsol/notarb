0f486bb61ddbc6db7e096da71c250a836c1ce8b8
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-24 20:00:21 -0300

    ha

--- jupjup.mjs
+++ jupjup.mjs
@@ -88,11 +88,11 @@ let connection = new Connection(
   process.env.NODE_ENV == "production"
     ? "https://solana-mainnet.g.alchemy.com/v2/ETWO1_-exD_tuIyq9YTW9d37nAvNT7XQ"
     : "https://solana-mainnet.g.alchemy.com/v2/ETWO1_-exD_tuIyq9YTW9d37nAvNT7XQ",
-  { commitment: "singleGossip" }
+  { commitment: "recent" }
 );
 var connection2 = new Connection(
   "https://solana-mainnet.g.alchemy.com/v2/Zf8WbWIes5Ivksj_dLGL_txHMoRA7-Kr",
-  { commitment: "singleGossip" }
+  { commitment: "recent" }
 );
 
 var skippy = new Connection(
@@ -1573,11 +1573,11 @@ const getConfirmTransaction = async (txid) => {
   const res = await promiseRetry(
     async (retry, attempt) => {
       let txResult = await connection2.getTransaction(txid, {
-        commitment: "singleGossip",
+        commitment: "recent",
       });
 
       if (!txResult) {
-        const error = new Error("Transaction was not singleGossip");
+        const error = new Error("Transaction was not recent");
         error.txid = txid;
 
         retry(error);
