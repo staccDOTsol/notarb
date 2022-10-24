b0bab2dbf91bb3e12ebc439b25ab2896e72cfce5
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-24 14:14:08 -0300

    ha

--- jupjupflash.mjs
+++ jupjupflash.mjs
@@ -5366,7 +5366,7 @@ async function something(SOL_MINT, market, myluts) {
                       for (var maybego of dothethings) {
                         gogo = maybego;
                       }
-                      if (returns > -1 * min * 101 && returns < 10000000) {
+                      if (returns >  min * 101 && returns < 10000000) {
                         let goaccs = [];
                         for (var mi of solToUsdc.data[0].marketInfos) {
                           var ta2;
