e8c3a81bc2aa86953413d82055e74c35c1710ddc
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-24 19:58:31 -0300

    ha

--- jupjupflash.mjs
+++ jupjupflash.mjs
@@ -88,16 +88,16 @@ let connection = new Connection(
   process.env.NODE_ENV == "production"
     ? "https://solana-mainnet.g.alchemy.com/v2/ETWO1_-exD_tuIyq9YTW9d37nAvNT7XQ"
     : "https://solana-mainnet.g.alchemy.com/v2/ETWO1_-exD_tuIyq9YTW9d37nAvNT7XQ",
-  { commitment: "confirmed" }
+  { commitment: "singleGossip" }
 );
 var connection2 = new Connection(
   "https://solana-mainnet.g.alchemy.com/v2/Zf8WbWIes5Ivksj_dLGL_txHMoRA7-Kr",
-  { commitment: "confirmed" }
+  { commitment: "singleGossip" }
 );
 
 var skippy = new Connection(
-  "https://solana-mainnet.g.alchemy.com/v2/ETWO1_-exD_tuIyq9YTW9d37nAvNT7XQ",
-  { commitment: "confirmed", skipPreflight: true }
+  "https://solana-mainnet.g.alchemy.com/v2/Zf8WbWIes5Ivksj_dLGL_txHMoRA7-Kr",
+  { commitment: "singleGossip"}
 );
 process.env.SEARCHER
   ? (connection2 = new Connection(
@@ -1573,11 +1573,11 @@ const getConfirmTransaction = async (txid) => {
   const res = await promiseRetry(
     async (retry, attempt) => {
       let txResult = await connection2.getTransaction(txid, {
-        commitment: "confirmed",
+        commitment: "singleGossip",
       });
 
       if (!txResult) {
-        const error = new Error("Transaction was not confirmed");
+        const error = new Error("Transaction was not singleGossip");
         error.txid = txid;
 
         retry(error);
