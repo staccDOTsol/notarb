a5c6cb8f38dfa726dc557a374f434e3ac0dd5373
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-24 22:37:42 -0300

    mm

--- jupjup.mjs
+++ jupjup.mjs
@@ -5666,7 +5666,7 @@ let                              messageV0 = new TransactionMessage({
                                   tokenAccount,
                                   tokenAccount,
                                   payer.publicKey,
-                                  Math.floor(myshit * 1.001)
+                                  Math.floor(myshit * 1.0001)
                                 )
                               );
                               var blockhash = await connection
