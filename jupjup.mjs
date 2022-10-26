afd2292e94be46374c320eceedb36e4a0775d432
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-26 15:41:03 -0300

    mm

--- jupjup.mjs
+++ jupjup.mjs
@@ -5287,7 +5287,7 @@ async function something(SOL_MINT, market, myluts) {
                     Math.floor(Math.floor(initial))
                   );
                   usdcToSol.data[0] = usdcToSol.data.find(
-                    (res) => res.marketInfos.length <= 3
+                    (res) => res.marketInfos.length <= 2
                   );
                 } catch (err) {
                   baddies.push(USDC_MINT + SOL_MINT);
