d2633f2d73f679658839d78195a8d84f931d34f2
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-27 10:30:36 -0300

    mm

--- jupjupflash.mjs
+++ jupjupflash.mjs
@@ -5243,7 +5243,7 @@ async function something(SOL_MINT, market, myluts) {
       if (!mints.includes(USDC_MINT)){
     //  mints.push(USDC_MINT)
      // console.log(mints.length)
-     // }     
+      }     
       var USDC_MINT = "8HGyAAB1yoM1ttS7pXjHMa3dukTFGQggnFFH3hJZgzQh"//reserve.config.liquidityToken.mint;
 
       if ( USDC_MINT == "8HGyAAB1yoM1ttS7pXjHMa3dukTFGQggnFFH3hJZgzQh"){//!baddies.includes(USDC_MINT+SOL_MINT) && !baddies.includes(SOL_MINT+USDC_MINT)) {
