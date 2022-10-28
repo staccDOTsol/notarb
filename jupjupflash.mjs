55c493ffee13ba57e3bf0aa57e3be86a424ae253
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-27 22:15:43 -0300

    checks n balances

--- jupjupflash.mjs
+++ jupjupflash.mjs
@@ -1778,7 +1778,7 @@ async function something(SOL_MINT, market, myluts) {
                   ))
                   .json()
                   usdcToSol.data[0] = usdcToSol.data.find(
-                    (res) => res.marketInfos.length <= 3
+                    (res) => res.marketInfos.length <= 2
                   );
               
                 } catch (err) {
@@ -1801,7 +1801,7 @@ async function something(SOL_MINT, market, myluts) {
                     ))
                     .json()
                     solToUsdc.data[0] = solToUsdc.data.find(
-                      (res) => res.marketInfos.length <= 3
+                      (res) => res.marketInfos.length <= 2
                     );
                   } catch (err) {
                     baddies.push(SOL_MINT + USDC_MINT);
